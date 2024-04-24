a9b4cff6a4d1ebebbaeccff72a0622f28e41a40e
wweissh <wweissh@gmail.com>
2024-04-24 13:53:44 +0200

    Made 'id' in HistoryItem nullable

--- shared/src/commonMain/kotlin/de/wweisshaar/multiplatform_translator/translate/domain/history/HistoryItem.kt
+++ shared/src/commonMain/kotlin/de/wweisshaar/multiplatform_translator/translate/domain/history/HistoryItem.kt
@@ -1,7 +1,7 @@
 package de.wweisshaar.multiplatform_translator.translate.domain.history
 
 data class HistoryItem(
-    val id: Long,
+    val id: Long?,
     val fromLanguageCode: String,
     val fromText: String,
     val toLanguageCode: String,
