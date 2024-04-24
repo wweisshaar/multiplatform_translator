7a0998e7fb18ebfa1a7dacbc747b3fb573cff98b
wweissh <wweissh@gmail.com>
2024-04-24 13:54:07 +0200

    Added Translate class with execute function in multiplatform_translator domain

---
+++ shared/src/commonMain/kotlin/de/wweisshaar/multiplatform_translator/translate/domain/translate/Translate.kt
@@ -0,0 +1,35 @@
+package de.wweisshaar.multiplatform_translator.translate.domain.translate
+
+import de.wweisshaar.multiplatform_translator.core.domain.language.Language
+import de.wweisshaar.multiplatform_translator.core.domain.util.Result
+import de.wweisshaar.multiplatform_translator.translate.domain.history.HistoryDataSource
+import de.wweisshaar.multiplatform_translator.translate.domain.history.HistoryItem
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language,
+    ): Result<String, TranslateError> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage = fromLanguage, fromText = fromText, toLanguage = toLanguage
+            )
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText
+                )
+            )
+            Result.Success(translatedText)
+        } catch (e: TranslateException) {
+            Result.Error(e.error)
+        }
+    }
+}
