b8d8dc32f5dbf51a5455d585a248a986be8f07b2
wweissh <wweissh@gmail.com>
2024-04-26 09:28:29 +0200

    Add AndroidTranslateViewModel wrapper in multiplatform translator android app

---
+++ androidApp/src/main/java/de/wweisshaar/multiplatform_translator/android/presentation/AndroidTranslateViewModel.kt
@@ -0,0 +1,31 @@
+package de.wweisshaar.multiplatform_translator.android.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.wweisshaar.multiplatform_translator.translate.domain.history.HistoryDataSource
+import de.wweisshaar.multiplatform_translator.translate.domain.translate.Translate
+import de.wweisshaar.multiplatform_translator.translate.presentation.TranslateEvent
+import de.wweisshaar.multiplatform_translator.translate.presentation.TranslateViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AndroidTranslateViewModel @Inject constructor(
+    private val translate: Translate,
+    private val historyDataSource: HistoryDataSource
+): ViewModel() {
+
+    private val viewModel by lazy {
+        TranslateViewModel(
+            translate = translate,
+            historyDataSource = historyDataSource,
+            coroutineScope = viewModelScope
+        )
+    }
+
+    val state = viewModel.state
+
+    fun onEvent(event: TranslateEvent) {
+        viewModel.onEvent(event)
+    }
+}
