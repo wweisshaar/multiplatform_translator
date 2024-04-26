b9f200fc35d033c62e27c446efded76c45cc525b
wweissh <wweissh@gmail.com>
2024-04-26 10:12:22 +0200

    Added Color.kt for defining light and dark color schemes to android app.

---
+++ androidApp/src/main/java/de/wweisshaar/multiplatform_translator/android/core/theme/Color.kt
@@ -0,0 +1,31 @@
+package de.wweisshaar.multiplatform_translator.android.core.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+import de.wweisshaar.multiplatform_translator.core.presentation.Colors
+
+
+val AccentViolet = Color(Colors.AccentViolet)
+val LightBlue = Color(Colors.LightBlue)
+val LightBlueGrey = Color(Colors.LightBlueGrey)
+val TextBlack = Color(Colors.TextBlack)
+val DarkGrey = Color(Colors.DarkGrey)
+
+val lightColors = lightColorScheme(
+    primary = AccentViolet,
+    background = LightBlueGrey,
+    onPrimary = Color.White,
+    onBackground = TextBlack,
+    surface = Color.White,
+    onSurface = TextBlack
+)
+
+val darkColors = darkColorScheme(
+    primary = AccentViolet,
+    background = DarkGrey,
+    onPrimary = Color.White,
+    onBackground = Color.White,
+    surface = DarkGrey,
+    onSurface = Color.White
+)
