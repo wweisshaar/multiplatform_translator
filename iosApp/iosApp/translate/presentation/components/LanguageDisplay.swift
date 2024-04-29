5a908c5676295b239d0daf0ff66f5e0c4a4d5de0
wweissh <wweissh@gmail.com>
2024-04-29 22:54:37 +0200

    Added LanguageDisplay component to iOS App.

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDisplay.swift
@@ -0,0 +1,28 @@
+//
+//  LanguageDisplay.swift
+//  iosApp
+//
+//  Created by Waldemar Weisshaar on 29.04.24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct LanguageDisplay: View {
+    var language: UiLanguage
+    var body: some View {
+        HStack {
+            SmallLanguageIcon(language: language)
+                .padding(.trailing, 5)
+            Text(language.language.langName)
+                .foregroundStyle(Color.lightBlue)
+        }
+    }
+}
+
+#Preview {
+    LanguageDisplay(
+        language: UiLanguage(language: .german, imageName: "german")
+    )
+}
