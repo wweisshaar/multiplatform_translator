9602bead981f23113b05d1d811cb493044851c4b
wweissh <wweissh@gmail.com>
2024-04-29 14:16:35 +0200

    Added subscribe method with GlobalScope and Main Dispatcher to CommonFlow.

--- shared/src/iosMain/kotlin/de/wweisshaar/multiplatform_translator/core/domain/util/CommonFlow.kt
+++ shared/src/iosMain/kotlin/de/wweisshaar/multiplatform_translator/core/domain/util/CommonFlow.kt
@@ -2,6 +2,8 @@ package de.wweisshaar.multiplatform_translator.core.domain.util
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
@@ -17,5 +19,15 @@ actual open class CommonFlow<T> actual constructor(
             flow.collect(onCollect)
         }
         return DisposableHandle { job.cancel() }
+    }
+
+    fun subscribe(
+        onCollect: (T) -> Unit
+    ): DisposableHandle {
+        return subscribe(
+            coroutineScope = GlobalScope,
+            dispatcher = Dispatchers.Main,
+            onCollect = onCollect
+        )
     }
 }
